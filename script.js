d0ede0595d6414803504cfdacb8655659dca2f9c
csrlpxg <csrlpxg@outlook.jp>
2024-12-07 20:43:15 +0800

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Redirecting...</title>
-<script>
-    window.onload = function() {
-        const targetUrl = 'https://ja.wikipedia.org/wiki/'; // 替换为目标 URL
-        document.getElementById('message').textContent = 'Webサイトの安全なジャンプ中...'; // 设置提示消息
+window.onload = function() {
+    const targetUrl = 'https://ja.wikipedia.org/wiki/';  // 替换为目标 URL
+    document.getElementById('message').textContent = 'Webサイトの安全なジャンプ中...';  // 设置提示消息为日文
 
-        // 延迟 1 秒后跳转
-        setTimeout(() => {
-            window.location.replace(targetUrl);  // 动态跳转
-        }, 1000);  // 延迟 1 秒
-    };
-</script>
-</head>
-<body>
-  <p id="message">Redirecting...</p>  <!-- 默认显示的消息 -->
-</body>
-</html>
+    // 延迟 1 秒后跳转
+    setTimeout(() => {
+        window.location.replace(targetUrl);  // 动态跳转
+    }, 1000);  // 延迟 1 秒
+};
